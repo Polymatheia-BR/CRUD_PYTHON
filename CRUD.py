595aeaacc16a4c91cefd295c1f2edc04861f56e4
Victor Ferreira <69025753+Polymatheia-BR@users.noreply.github.com>
2023-11-16 23:09:46 -0300

    Update CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -1,41 +1,60 @@
 import mysql.connector
+import os
 
-def mostrarDB(cursor):
-    cursor.execute("SHOW DATABASES")
-    print(f"Os Bancos de dados são: ", end='')
-    for database in cursor:
-        print(database[0], end='|')
-    print()
+limpar_tela = lambda: os.system('cls')
+conexao = mysql.connector.connect(user='root', password='302302', host='localhost', database='academia')
+cursor = conexao.cursor()
 
-def criarDB(cursor, dbname): 
-    if not verificarSeDBExiste(cursor, dbname):
-        cursor.execute(f"CREATE DATABASE {dbname}")
-        print(f'O banco de dados {dbname} foi criado com exito')
-    else:
-        print(f"ERRO: Um banco de dados com o nome {dbname} já existe")
+#Retorna o id da entidade criada
+def inserir_dados():
+  valores = ['nome','CPF','telefone','endereco']
+  for i in range(len(valores)):
+    valores[i] = input(f'Digite um valor para a coluna {valores[i]}:')
+  cursor.execute('INSERT INTO aluno (nome, CPF, telefone, endereco) VALUES (%s,%s,%s,%s)', tuple(valores))
+  conexao.commit()
+  print(f'O aluno com o ID {cursor.lastrowid} foi criado')
 
-def verificarSeDBExiste(cursor, nome):
-    nome = nome.lower()
-    cursor.execute('SHOW DATABASES')
-    for database in cursor:
-        if database[0] == nome:
-            return True
-    return False
+def mostrar_alunos():
+  print('TABELA ALUNO')
+  print(f'MATRICULA|{"NOME":<30}|{"CPF":<11}|{"TELEFONE":<11}|ENDEREÇO')
+  cursor.execute('SELECT * FROM aluno')
+  for (matricula, nome, CPF, telefone, endereco) in cursor:
+    print(f'{matricula:<9}|{nome:<30}|{CPF:<11}|{telefone:<11}|{endereco}')
 
-def deletarDB(cursor, dbname):
-    if verificarSeDBExiste(cursor, dbname):
-        cursor.execute(f'DROP DATABASE {dbname}')
-        print(f'O banco de dados {dbname} foi deletado com exito')
-    else:
-        print(f'ERRO: Não foi encontrado um banco de dados com o nome de {dbname}!')
+def puxar_dados(matricula_query):
+  cursor.execute(f'SELECT * FROM aluno WHERE matricula = {matricula_query}')
+  return cursor.fetchall()[0]
 
-conexao = mysql.connector.connect(user='root', password='302302', host='127.0.0.1')
-cursor = conexao.cursor(buffered=True)
+def atualizar_dados():
+  matricula = int(input('Digite a matricula de um Aluno: '))
+  aluno = puxar_dados(matricula)
+  nome = input('Digite o novo nome do aluno: ') or aluno[1]
+  CPF = input('Digite o novo CPF: ') or aluno[2]
+  telefone = input('Digite o novo telefone: ') or aluno[3]
+  endereco = input('Digite o novo endereço: ') or aluno[4]
+  cursor.execute('UPDATE aluno SET nome = %s, CPF = %s, telefone = %s, endereco = %s WHERE matricula = %s',(nome, CPF, telefone, endereco, matricula))
+  conexao.commit()
 
-mostrarDB(cursor)
-criarDB(cursor, 'academia')
-mostrarDB(cursor)
-deletarDB(cursor, 'academia')
-mostrarDB(cursor)
+def deletar_dados(matricula):
+  cursor.execute('DELETE FROM aluno WHERE matricula = %s', (matricula))
+  conexao.commit()
+  print(f'Aluno com matricula {matricula} deletado com exito.')
+
+while True:
+  menu = int(input(f'==========\n1 - Inserir Dados\n2 - Ler dados\n3 - Atualizar Dados\n4 - Deletar Dados\n5 - Sair\nSelecione uma opção: '))
+  limpar_tela()
+  match menu:
+    case 1:
+      inserir_dados()
+    case 2:
+      mostrar_alunos()
+    case 3:
+      mostrar_alunos()
+      atualizar_dados()
+    case 4:
+      mostrar_alunos()
+      deletar_dados(int(input('Digite a matricula do aluno: ')))
+    case 5:
+      break
 
 conexao.close()
